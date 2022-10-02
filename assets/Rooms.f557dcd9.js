e07ae4dac87b872b1a979a286f84d6514e631b29
GuillaumeChk <guillaume.chekhab39@gmail.com>
2022-10-02 18:48:14 +0200

    update dist

---
+++ assets/Rooms.f557dcd9.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as _}from"./index.c923fa2d.js";const t={},c={class:"rooms"},n=_("h1",null,"This is an rooms page",-1),a=[n];function r(i,d){return o(),s("div",c,a)}const m=e(t,[["render",r]]);export{m as default};
