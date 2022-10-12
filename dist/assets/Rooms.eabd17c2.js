5bbfe8d80e0a1f9656a1c49cecc1a24621900137
GuillaumeChk <guillaume.chekhab39@gmail.com>
2022-10-12 20:27:36 +0200

    initial dist subtree comit

---
+++ dist/assets/Rooms.eabd17c2.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as _}from"./index.6938427d.js";const t={},c={class:"rooms"},n=_("h1",null,"This is an rooms page",-1),a=[n];function r(i,d){return o(),s("div",c,a)}const m=e(t,[["render",r]]);export{m as default};
