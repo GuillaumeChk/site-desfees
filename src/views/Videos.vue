1f441db53770eeafbf742f83ba02f69b81e02616
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-01-05 18:10:41 +0100

    add videos

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -24,151 +24,80 @@
 				{{ $t("gallery.titre9") }}
 			</h4> -->
 
-			<section class="q-gutter-y-md">
-				<h5 class="q-px-md q-pt-md">{{ $t("gallery.titre10") }}</h5>
-
-				<q-responsive
-					:ratio="16 / 9"
-					style="height: 600px; max-width: 100%; margin-bottom: 100px"
-				>
-					<q-carousel
-						swipeable
-						animated
-						arrows
-						infinite
-						thumbnails
-						v-model="slide"
-						v-model:fullscreen="fullscreen"
-						class="uncropped-image"
-						control-color="black"
-					>
-						<q-carousel-slide
-							v-for="(slide, key, index) in imagesInterieur"
-							:key="key"
-							:name="index"
-							:img-src="slide"
-							class="uncropped-image"
-						/>
-
-						<template v-slot:control>
-							<q-carousel-control position="bottom-right" :offset="[18, 18]">
-								<q-btn
-									round
-									flat
-									color="white"
-									text-color="black"
-									:icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
-									@click="fullscreen = !fullscreen"
-								/>
-							</q-carousel-control>
-						</template>
-					</q-carousel>
+			<section class="q-py-xl q-gutter-lg justify-center">
+				<q-responsive style="height: 600px; max-width: 100%">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube-nocookie.com/embed/qpZ8VSYXky4"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					></iframe>
 				</q-responsive>
-				<h5 class="q-px-md q-pt-md">{{ $t("gallery.titre11") }}</h5>
-				<q-responsive
-					:ratio="16 / 9"
-					style="height: 600px; max-width: 100%; margin-bottom: 100px"
-				>
-					<q-carousel
-						swipeable
-						animated
-						arrows
-						infinite
-						thumbnails
-						v-model="slide2"
-						v-model:fullscreen="fullscreen2"
-						class="uncropped-image"
-						control-color="black"
-					>
-						<q-carousel-slide
-							v-for="(slide2, key, index) in imagesExterieur"
-							:key="key"
-							:name="index"
-							:img-src="slide2"
-							class="uncropped-image"
-						/>
 
-						<template v-slot:control>
-							<q-carousel-control position="bottom-right" :offset="[18, 18]">
-								<q-btn
-									round
-									flat
-									color="white"
-									text-color="black"
-									:icon="fullscreen2 ? 'fullscreen_exit' : 'fullscreen'"
-									@click="fullscreen2 = !fullscreen2"
-								/>
-							</q-carousel-control>
-						</template>
-					</q-carousel>
-				</q-responsive>
-				<h5 class="q-px-md q-pt-md">{{ $t("gallery.titre12") }}</h5>
-				<q-responsive
-					:ratio="16 / 9"
-					style="height: 600px; max-width: 100%; margin-bottom: 100px"
-				>
-					<q-carousel
-						swipeable
-						animated
-						arrows
-						infinite
-						thumbnails
-						v-model="slide3"
-						v-model:fullscreen="fullscreen3"
-						class="uncropped-image"
-						control-color="black"
-					>
-						<q-carousel-slide
-							v-for="(slide3, key, index) in imagesJura"
-							:key="key"
-							:name="index"
-							:img-src="slide3"
-							class="uncropped-image"
-						/>
-
-						<template v-slot:control>
-							<q-carousel-control position="bottom-right" :offset="[18, 18]">
-								<q-btn
-									round
-									flat
-									color="white"
-									text-color="black"
-									:icon="fullscreen3 ? 'fullscreen_exit' : 'fullscreen'"
-									@click="fullscreen3 = !fullscreen3"
-								/>
-							</q-carousel-control>
-						</template>
-					</q-carousel>
-				</q-responsive>
+				<q-responsive style="height: 600px; max-width: 100%">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube-nocookie.com/embed/ZKSqWQTjp7Q"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					></iframe
+				></q-responsive>
+				<q-responsive style="height: 600px; max-width: 100%">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube-nocookie.com/embed/wXMpA4vJJl0"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					></iframe
+				></q-responsive>
+				<q-responsive style="height: 600px; max-width: 100%">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube-nocookie.com/embed/Qm46PlLpwHw"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					></iframe
+				></q-responsive>
+				<q-responsive style="height: 600px; max-width: 100%">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube-nocookie.com/embed/X8i-l-WtUZw"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					></iframe
+				></q-responsive>
+				<q-responsive style="height: 600px; max-width: 100%">
+					<iframe
+						width="560"
+						height="315"
+						src="https://www.youtube-nocookie.com/embed/N04lsB6VOQE"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					></iframe
+				></q-responsive>
 			</section>
 		</div>
 	</q-page>
 </template>
 
-<script setup>
-import CustomDivider from "../components/CustomDivider.vue";
-import { ref } from "vue";
-
-const slide = ref(0);
-const slide2 = ref(0);
-const slide3 = ref(0);
-const fullscreen = ref(false);
-const fullscreen2 = ref(false);
-const fullscreen3 = ref(false);
-
-const imagesInterieur = import.meta.glob(
-	"../assets/rooms/interieur/*.(jpg|JPG|png|PNG)",
-	{ as: "url", eager: true }
-);
-const imagesExterieur = import.meta.glob(
-	"../assets/rooms/exterieur/*.(jpg|JPG|png|PNG)",
-	{ as: "url", eager: true }
-);
-const imagesJura = import.meta.glob(
-	"../assets/rooms/jura/*.(jpg|JPG|png|PNG)",
-	{ as: "url", eager: true }
-);
-</script>
+<script setup></script>
 
 <style>
 .uncropped-image {
